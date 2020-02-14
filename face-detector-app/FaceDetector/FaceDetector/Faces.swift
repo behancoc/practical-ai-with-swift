b9fd003b491659319da9731f8c660a84c0e7c3f0
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-14 12:51:53 -0800

    class with extension to detect faces in a UIImage using bounding box

--- face-detector-app/FaceDetector/FaceDetector/Faces.swift
+++ face-detector-app/FaceDetector/FaceDetector/Faces.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2020 Brandon Hancock. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Vision
+
+extension UIImage {
+    func detectFaces(completion: @escaping([VNFaceObservation]?) -> ()) {
+        guard let image = self.cgImage else { return completion(nil) }
+        
+        let request = VNDetectFaceRectangleRequest()
+        
+        DispatchQueue.global().async {
+            let handler = VNImageRequestHandler(cgImage: image,
+                                                orientation: self.cgImageOrientation)
+            
+            try?handler.perform([request])
+            
+            guard let observations = request.results as? [VNFaceObservation] else {
+                return completion(nil)
+            }
+            completion(observations)
+        }
+    }
+}
