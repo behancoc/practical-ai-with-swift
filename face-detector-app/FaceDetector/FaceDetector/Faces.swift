e195836d07b7c0b3146afcc2422b922dc1f9ba7f
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-14 14:48:18 -0800

    Corrects issues within Faces.swift file

--- face-detector-app/FaceDetector/FaceDetector/Faces.swift
+++ face-detector-app/FaceDetector/FaceDetector/Faces.swift
@@ -13,7 +13,7 @@ extension UIImage {
     func detectFaces(completion: @escaping([VNFaceObservation]?) -> ()) {
         guard let image = self.cgImage else { return completion(nil) }
         
-        let request = VNDetectFaceRectangleRequest()
+        let request = VNDetectFaceRectanglesRequest()
         
         DispatchQueue.global().async {
             let handler = VNImageRequestHandler(cgImage: image,
